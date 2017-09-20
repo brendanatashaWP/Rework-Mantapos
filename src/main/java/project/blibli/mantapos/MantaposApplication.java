107cd99ecbf1d832b2d2c443567b5a30a5f63d18
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-09-20 19:33:44 +0700

    first commit

---
+++ src/main/java/project/blibli/mantapos/MantaposApplication.java
@@ -0,0 +1,12 @@
+package project.blibli.mantapos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantaposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantaposApplication.class, args);
+	}
+}
---
+++ src/test/java/project/blibli/mantapos/MantaposApplicationTests.java
@@ -0,0 +1,16 @@
+package project.blibli.mantapos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MantaposApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
