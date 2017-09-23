98555ce754a6d065dcbfedcdaea36ad96ce4056f
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-09-23 16:06:42 +0700

    - Melengkapi beans_model user

--- src/main/java/project/blibli/mantapos/Beans_Model/User.java
+++ src/main/java/project/blibli/mantapos/Beans_Model/User.java
@@ -3,6 +3,7 @@ package project.blibli.mantapos.Beans_Model;
 public class User {
     private int id;
     private String username, password, role, status;
+    private String nama_lengkap, nomor_ktp, nomor_telepon, alamat;
     private int id_restaurant;
 
     public int getId() {
@@ -43,6 +44,38 @@ public class User {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getNama_lengkap() {
+        return nama_lengkap;
+    }
+
+    public void setNama_lengkap(String nama_lengkap) {
+        this.nama_lengkap = nama_lengkap;
+    }
+
+    public String getNomor_ktp() {
+        return nomor_ktp;
+    }
+
+    public void setNomor_ktp(String nomor_ktp) {
+        this.nomor_ktp = nomor_ktp;
+    }
+
+    public String getNomor_telepon() {
+        return nomor_telepon;
+    }
+
+    public void setNomor_telepon(String nomor_telepon) {
+        this.nomor_telepon = nomor_telepon;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
     }
 
     public int getId_restaurant() {
