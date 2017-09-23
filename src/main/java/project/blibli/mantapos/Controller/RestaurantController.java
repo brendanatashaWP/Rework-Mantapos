b82c689dedb0a20852fcccbe62c56ce354a7590e
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-09-23 17:37:49 +0700

    - Sudah bisa insert restaurant (return json) - Sudah ada bean validation restaurant

--- src/main/java/project/blibli/mantapos/Controller/RestaurantController.java
+++ src/main/java/project/blibli/mantapos/Controller/RestaurantController.java
@@ -1,26 +1,29 @@
 package project.blibli.mantapos.Controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.blibli.mantapos.Beans_Model.Restaurant;
 import project.blibli.mantapos.Dao.RestaurantDao;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class RestaurantController {
     @PostMapping(value = "/add-restaurant", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> addRestoJson(@ModelAttribute("restaurant")Restaurant restaurant){
+    public Map<String, String> addRestoJson(@Valid @ModelAttribute("restaurant") Restaurant restaurant,
+                                            BindingResult bindingResult){
         Map<String, String> param = new HashMap<>();
         int status = RestaurantDao.Insert(restaurant);
         if (status==1){
             param.put("status", "success!");
             param.put("resto_name", restaurant.getRestaurantName());
             param.put("resto_address", restaurant.getRestaurantAddress());
-        } else{
+        } else if(bindingResult.hasErrors()){
             param.put("status", "failed");
         }
         return param;
