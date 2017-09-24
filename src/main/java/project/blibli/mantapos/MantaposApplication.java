33fe6a10ff5043dcaf4c1b5a2e58c6c33ccf5355
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-09-24 21:24:27 +0700

    test coba

--- src/main/java/project/blibli/mantapos/MantaposApplication.java
+++ src/main/java/project/blibli/mantapos/MantaposApplication.java
@@ -1,18 +1,51 @@
 package project.blibli.mantapos;
 
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import project.blibli.mantapos.Dao.*;
 
+import java.io.File;
+
 @SpringBootApplication
 public class MantaposApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MantaposApplication.class, args);
+		String webappDirLocation = "src/main/webapp/";
+		Tomcat tomcat = new Tomcat();
+
+		//The port that we should run on can be set into an environment variable
+		//Look for that variable and default to 8080 if it isn't there.
+		String webPort = System.getenv("PORT");
+		if(webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
+
+		tomcat.setPort(Integer.valueOf(webPort));
+
+		StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+		System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+		// Declare an alternative location for your "WEB-INF/classes" dir
+		// Servlet 3.0 annotation will work
+		File additionWebInfClasses = new File("target/classes");
+		WebResourceRoot resources = new StandardRoot(ctx);
+		resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/jsp/classes",
+				additionWebInfClasses.getAbsolutePath(), "/"));
+		ctx.setResources(resources);
+
+		tomcat.start();
+		tomcat.getServer().await();
 		MenuDao.CreateTable();
 		OrderDao.CreateTable();
 		OrderedMenuDao.CreateTable();
 		RestaurantDao.CreateTable();
 		UserDao.CreateTable();
 	}
+
 }
