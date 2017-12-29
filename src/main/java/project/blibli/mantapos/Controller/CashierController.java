04dd8c0fe04c64c80e6149827f10a6bed3f6f2aa
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-12-29 21:49:18 +0700

    Service cashier sudah oke

--- src/main/java/project/blibli/mantapos/Controller/CashierController.java
+++ src/main/java/project/blibli/mantapos/Controller/CashierController.java
@@ -2,57 +2,32 @@ package project.blibli.mantapos.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 import project.blibli.mantapos.Config.Mail;
 import project.blibli.mantapos.Model.*;
-import project.blibli.mantapos.ImplementationDao.*;
-import project.blibli.mantapos.WeekGenerator;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import project.blibli.mantapos.Service.CashierService;
 
 @RestController
 //Controller untuk cashier (akses URL /cashier, penerimaan order baru, dan pengiriman receipt melalui email)
 public class CashierController {
 
-    MenuDaoImpl menuDao = new MenuDaoImpl();
-    LedgerDaoImpl ledgerDao = new LedgerDaoImpl();
-    SaldoDaoImpl saldoDao = new SaldoDaoImpl();
-    MenuYangDipesanDaoImpl orderedMenuDao = new MenuYangDipesanDaoImpl();
-    RestoranDaoImpl restaurantDao = new RestoranDaoImpl();
-    Restoran restoran;
-    Saldo saldo = new Saldo();
-
     Mail mail = new Mail();
 
-    //Melakukan AutoWired untuk templateEngine, ini digunakan untuk mengirim email dalam bentuk HTML
-    @Autowired
+    CashierService cashierService;
     TemplateEngine templateEngine;
 
-    //Mapping untuk akses URL /cashier
+    @Autowired
+    public CashierController(CashierService cashierService, TemplateEngine templateEngine){
+        this.cashierService = cashierService;
+        this.templateEngine = templateEngine;
+    }
+
     @GetMapping(value = "/cashier", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView cashierHtml(Authentication authentication){
-        ModelAndView mav = new ModelAndView();
-
-        String loggedInUsername = authentication.getName(); //Ambil username yang sedang login
-        mav.addObject("loggedInUsername", loggedInUsername);
-
-        restoran = restaurantDao.readOne(restaurantDao.readIdRestoBasedOnUsernameRestoTerkait(loggedInUsername)); //Mengambil informasi restoran berdasarkan nama username yang login (username itu belong ke restoran mana)
-        mav.addObject("restoran", restoran); //object restoran ini gunanya nanti untuk di receipt
-
-        List<Menu> menuList = menuDao.readAll(restoran.getId(), 0, 0); //Mengambil semua menu yang ada di database. itemPerPage dan page dibuat 0 karena tidak akan memakai pagination
-        mav.setViewName("cashier");
-        mav.addObject("menuList", menuList);
-        return mav;
+        return cashierService.getMappingCashier(authentication);
     }
 
     //Mapping untuk menambah order dari cashier ke database
@@ -65,71 +40,6 @@ public class CashierController {
                                      @RequestParam("namaResto") String nama_resto,
                                      @RequestParam("customer_name") String nama_customer,
                                      Authentication authentication){
-        ModelAndView mav = new ModelAndView();
-
-        String loggedInUsername = authentication.getName(); //Ambil username yang sedang login
-        int id_resto = restaurantDao.readIdRestoBasedOnUsernameRestoTerkait(loggedInUsername);
-        System.out.println("id resto : " + id_resto);
-//        restoran = restaurantDao.GetRestaurantInfo(loggedInUsername); //Mengambil informasi restoran berdasarkan username yg login (username itu belong ke restoran mana)
-//        int id_resto = restoran.getId(); //Mengambil id restoran yang didapat dari hasil di atas
-        int tanggal = LocalDate.now().getDayOfMonth();
-        int bulan = LocalDate.now().getMonthValue();
-        int tahun = LocalDate.now().getYear();
-        ledger.setTipe("debit"); ledger.setKeperluan("penjualan menu"); //Karena disini adalah pemasukkan, makannya tipe ledger adalah debit dan keperluannya adalah penjualan menu
-        ledger.setWaktu(LocalDate.now().toString()); //setWaktu untuk di-insert ke database adalah waktu sekarang (yyyy-mm-dd)
-        ledger.setTanggal(tanggal); //setTanggal
-        ledger.setWeek(WeekGenerator.GetWeek(LocalDateTime.now().getDayOfMonth())); //setWeek, value week diambil dari tanggalnya. Detailnya ada di class WeekGenerator
-        ledger.setMonth(bulan); //setMonth
-        ledger.setYear(tahun); //setYear
-        ledgerDao.insert(ledger, restoran.getId()); //insert informasi pemesanan ke database (tabel ledger_harian)
-
-        saldo.setId_resto(id_resto);
-        saldo.setTipe_saldo("akhir");
-        saldo.setTanggal(tanggal);
-        saldo.setMonth(bulan);
-        saldo.setYear(tahun);
-        saldo.setSaldo(saldoDao.getSaldoAwal(id_resto) + ledgerDao.getTotalDebitBulanan(id_resto, bulan, tahun) - ledgerDao.getTotalKreditBulanan(id_resto, bulan, tahun)); //saldo akhir = saldo awal + debit - kredit
-        saldoDao.insert(saldo, id_resto);
-
-        int lastOrderId = ledgerDao.getLastId(id_resto); //Mengambil id dari order yang barusan dimasukkan (record terakhir). ID nya ini untuk dijadikan foreign key di table OrderedMenu
-        //Melakukan for dari 0 hingga panjang dari Array array_id_order. Array ini isinya adalah id dari menu2 yang dipesan. Memasukkan id menu2 itu ada di order.js
-        for(int i=0; i<array_id_order.length; i++){
-            orderedMenuDao.insertMenuYangDipesan(lastOrderId, Integer.parseInt(array_id_order[i]),
-                    Integer.parseInt(array_qty[i]));
-            //Melakukan insert ke table OrderedMenu. Dimana foreign key nya adalah id dari last Order (record terakhir tadi).
-            //lalu id dari tiap2 menu yang dipesan itu diinsert ke table OrderedMenu, bersamaan dengan quantity-nya dari Array array_qty. Memasukkan quantity-nya ada di order.js
-        }
-
-        //Melakukan pengecekan apakah customer ingin receipt dikirim via email atau tidak. Di-pass dari HTML kesini dalam variabel is_kirim_email_receipt
-        if(is_kirim_email_receipt.equals("yes")){ //jika YES
-            MimeMessage maill = mail.send().createMimeMessage();
-            try {
-                MimeMessageHelper helper = new MimeMessageHelper(maill, true);
-                Context context = new Context();
-                helper.setFrom("mantapos@axella.online");
-                helper.setTo(email_kirim_receipt); //Alamat tujuan pengiriman email, di-pass dari HTML kesini dalam variabel email_kirim_receipt
-                helper.setSubject("RECEIPT PEMBELIAN DI " + nama_resto); //Subject email
-                context.setVariable("nama_customer", nama_customer); //Melempar nama customer ke HTML receipt (email.html)
-                context.setVariable("namaResto", nama_resto); //Melempar nama restoran ke HTML receipt (email.html)
-                context.setVariable("tanggal", LocalDate.now().toString()); //Melempar tanggal sekarang ke HTML receipt (email.html)
-                context.setVariable("total_harga", ledger.getBiaya()); //Melempar total biaya ke HTML receipt (email.html). Total biaya didapat dari object ledger yang dilempar dari kasir kesini.
-                List<OrderedMenu> ordered_menu_list = new ArrayList<>(); //List yang berisi detail menu (nama menu, harga menu, qty, dan total harga menu)
-                for (int i=0; i<array_id_order.length; i++){
-                    Menu menuObject = menuDao.readOne(Integer.parseInt(array_id_order[i])); //Mengambil detail dari masing-masing menu. Cara mengambil detailnya adalah berdasarkan id menu yang ada di Array array_id_order
-                    ordered_menu_list.add(new OrderedMenu(
-                            menuObject.getNama_menu(),
-                            (menuObject.getHarga_menu() * Integer.parseInt(array_qty[i])),
-                            Integer.parseInt(array_qty[i])));
-                }
-                context.setVariable("ordered_menu_list", ordered_menu_list); //Melempar list ordered_menu_list yang berisi detail menu yang di-order ke HTML receipt (email.html)
-                String body = templateEngine.process("email", context); //Menspecify body dari email adalah email.html dengan context (yaitu variabel2 yang dilempar tadi)
-                helper.setText(body, true);
-                mail.send().send(maill); //Melakukan send email. Harap cek spam untuk melihat email yang dikirim. Tidak tahu kenapa masuk spam.
-            } catch (MessagingException e) {
-                e.printStackTrace();
-            }
-        }
-        mav.setViewName("redirect:/cashier");
-        return mav;
+        return cashierService.postMappingCashier(ledger, array_id_order, array_qty, is_kirim_email_receipt, email_kirim_receipt, nama_resto, nama_customer, authentication, templateEngine, mail);
     }
 }
---
+++ src/main/java/project/blibli/mantapos/Service/CashierService.java
@@ -0,0 +1,155 @@
+package project.blibli.mantapos.Service;
+
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import project.blibli.mantapos.Config.Mail;
+import project.blibli.mantapos.ImplementationDao.*;
+import project.blibli.mantapos.Model.*;
+import project.blibli.mantapos.WeekGenerator;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CashierService {
+
+    LedgerDaoImpl ledgerDao = new LedgerDaoImpl();
+    SaldoDaoImpl saldoDao = new SaldoDaoImpl();
+    RestoranDaoImpl restoranDao = new RestoranDaoImpl();
+    MenuDaoImpl menuDao = new MenuDaoImpl();
+    MenuYangDipesanDaoImpl menuYangDipesanDao = new MenuYangDipesanDaoImpl();
+
+    int tanggal = LocalDate.now().getDayOfMonth();
+    int bulan = LocalDate.now().getMonthValue();
+    int tahun = LocalDate.now().getYear();
+
+    public ModelAndView getMappingCashier(Authentication authentication){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("cashier");
+        String loggedInUsername = getLoggedInUsername(authentication);
+        int idResto = getIdRestoBasedOnUsernameTerkait(loggedInUsername);
+        mav.addObject("loggedInUsername", loggedInUsername);
+        mav.addObject("restoran", getInfoRestoran(idResto));
+        mav.addObject("menuList", getAllMenu(idResto));
+        return mav;
+    }
+
+    public ModelAndView postMappingCashier(Ledger ledger,
+                                           String[] arrayIdMenu,
+                                           String[] arrayQtyMenu,
+                                           String apakahMauKirimReceiptMelaluiEmail,
+                                           String emailTujuan,
+                                           String namaResto,
+                                           String namaCustomer,
+                                           Authentication authentication,
+                                           TemplateEngine templateEngine,
+                                           Mail mail){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("redirect:/cashier");
+        String loggedInUsername = authentication.getName();
+        int idResto = getIdRestoBasedOnUsernameTerkait(loggedInUsername);
+        insertPemasukkan(ledger, idResto);
+        updateSaldoAkhir(idResto);
+        insertDetailMenuYangDipesan(getLastIdOrder(idResto), arrayIdMenu, arrayQtyMenu);
+        if(apakahMauKirimReceiptMelaluiEmail.equals("yes")){
+            kirimReceiptMelaluiEmail(emailTujuan, namaResto, namaCustomer, templateEngine, mail, ledger.getBiaya(), arrayIdMenu, arrayQtyMenu);
+        }
+        return mav;
+    }
+
+    public List<Menu> getAllMenu(int idResto){
+        List<Menu> menuList = menuDao.readAll(idResto, 0, 0);
+        return menuList;
+    }
+
+    public Restoran getInfoRestoran(int idResto){
+        Restoran restoran = restoranDao.readOne(idResto);
+        return restoran;
+    }
+
+    public int getIdRestoBasedOnUsernameTerkait(String username){
+        int idResto = restoranDao.readIdRestoBasedOnUsernameRestoTerkait(username);
+        return idResto;
+    }
+
+    public String getLoggedInUsername(Authentication authentication){
+        String username = authentication.getName();
+        return username;
+    }
+
+    public int getLastIdOrder(int idResto){
+        int lastIdOrder = ledgerDao.getLastId(idResto);
+        return lastIdOrder;
+    }
+
+    public void insertDetailMenuYangDipesan(int idOrder, String[] arrayIdMenu, String[] arrayQtyMenu){
+        for (int i=0; i<arrayIdMenu.length; i++){
+            menuYangDipesanDao.insertMenuYangDipesan(idOrder, Integer.parseInt(arrayIdMenu[i]), Integer.parseInt(arrayQtyMenu[i]));
+        }
+    }
+
+    public void insertPemasukkan(Ledger ledger, int idResto){
+        ledger.setTipe("debit");
+        ledger.setKeperluan("penjualan menu"); //Karena disini adalah pemasukkan, makannya tipe ledger adalah debit dan keperluannya adalah penjualan menu
+        ledger.setWaktu(LocalDate.now().toString()); //setWaktu untuk di-insert ke database adalah waktu sekarang (yyyy-mm-dd)
+        ledger.setTanggal(tanggal); //setTanggal
+        ledger.setWeek(WeekGenerator.GetWeek(LocalDateTime.now().getDayOfMonth())); //setWeek, value week diambil dari tanggalnya. Detailnya ada di class WeekGenerator
+        ledger.setMonth(bulan); //setMonth
+        ledger.setYear(tahun); //setYear
+        ledgerDao.insert(ledger, idResto);
+    }
+
+    public void updateSaldoAkhir(int idResto){
+        Saldo saldo = new Saldo();
+        saldo.setId_resto(idResto);
+        saldo.setTipe_saldo("akhir");
+        saldo.setMonth(bulan);
+        saldo.setYear(tahun);
+        saldo.setSaldo(saldoDao.getSaldoAwal(idResto) + ledgerDao.getTotalDebitBulanan(idResto, bulan, tahun) - ledgerDao.getTotalKreditBulanan(idResto, bulan, tahun)); //saldo akhir = saldo awal + debit - kredit
+    }
+
+    public void kirimReceiptMelaluiEmail(String alamatEmailTujuan,
+                                         String namaResto,
+                                         String namaCustomer,
+                                         TemplateEngine templateEngine,
+                                         Mail mail,
+                                         int totalBiaya,
+                                         String[] arrayIdMenu,
+                                         String[] arrayQtyMenu){
+        MimeMessage maill = mail.send().createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(maill, true);
+            Context context = new Context();
+            helper.setFrom("mantapos@axella.online");
+            helper.setTo(alamatEmailTujuan); //Alamat tujuan pengiriman email, di-pass dari HTML kesini dalam variabel email_kirim_receipt
+            helper.setSubject("RECEIPT PEMBELIAN DI " + namaResto); //Subject email
+            context.setVariable("nama_customer", namaCustomer); //Melempar nama customer ke HTML receipt (email.html)
+            context.setVariable("namaResto", namaResto); //Melempar nama restoran ke HTML receipt (email.html)
+            context.setVariable("tanggal", LocalDate.now().toString()); //Melempar tanggal sekarang ke HTML receipt (email.html)
+            context.setVariable("total_harga", totalBiaya); //Melempar total biaya ke HTML receipt (email.html). Total biaya didapat dari object ledger yang dilempar dari kasir kesini.
+            List<OrderedMenu> ordered_menu_list = new ArrayList<>(); //List yang berisi detail menu (nama menu, harga menu, qty, dan total harga menu)
+            for (int i=0; i<arrayIdMenu.length; i++){
+                Menu menuObject = menuDao.readOne(Integer.parseInt(arrayIdMenu[i])); //Mengambil detail dari masing-masing menu. Cara mengambil detailnya adalah berdasarkan id menu yang ada di Array array_id_order
+                ordered_menu_list.add(new OrderedMenu(
+                        menuObject.getNama_menu(),
+                        (menuObject.getHarga_menu() * Integer.parseInt(arrayQtyMenu[i])),
+                        Integer.parseInt(arrayQtyMenu[i])));
+            }
+            context.setVariable("ordered_menu_list", ordered_menu_list); //Melempar list ordered_menu_list yang berisi detail menu yang di-order ke HTML receipt (email.html)
+            String body = templateEngine.process("email", context); //Menspecify body dari email adalah email.html dengan context (yaitu variabel2 yang dilempar tadi)
+            helper.setText(body, true);
+            mail.send().send(maill); //Melakukan send email. Harap cek spam untuk melihat email yang dikirim. Tidak tahu kenapa masuk spam.
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
