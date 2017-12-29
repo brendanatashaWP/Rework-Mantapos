ed130f0d3abae148a2355d496e206fc77ca99661
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-12-29 21:55:32 +0700

    Service Login sudah okay

--- src/main/java/project/blibli/mantapos/Controller/LoginController.java
+++ src/main/java/project/blibli/mantapos/Controller/LoginController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import project.blibli.mantapos.Config.Mail;
+import project.blibli.mantapos.Service.LoginService;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -26,45 +27,30 @@ import java.util.Map;
 //Akses ke URL /privilege, yaitu jika user mengakses page yang tidak seharusnya (role-nya tidak sesuai)
 public class LoginController {
 
+    LoginService loginService;
+
+    @Autowired
+    public LoginController (LoginService loginService){
+        this.loginService = loginService;
+    }
+
     //Jika user mengakses URL /login
     @GetMapping(value = "/login", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView loginHtml(@RequestParam(value = "error", required = false) String error,
                                   @RequestParam(value = "logout", required = false) String logout){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("login");
-        //jika dalam URL itu ada parameter error (/login?error), maka ada error, maka tambahkan object error ke mav. Nantinya akan di-check di login.html
-        if(error!=null)
-            mav.addObject("error", "Invalid username and/or password");
-        //jika dalam URL itu ada parameter logout (/login?logout), maka user berhasil logout, maka tambahkan object logout ke mav. Nantinya akan di-check di login.html
-        else if(logout!=null)
-            mav.addObject("logout", "You've been logged out successfully!");
-        return mav;
+        return loginService.getMappingLogin(error, logout);
     }
 
     //Melakukan assigning default page untuk user yang login berdasarkan role-nya
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView index(Authentication authentication){
-        ModelAndView mav = new ModelAndView();
-        if(authentication.getAuthorities().toString().equals("[cashier]"))
-            mav.setViewName("redirect:/cashier");
-        else if(authentication.getAuthorities().toString().equals("[manager]") || authentication.getAuthorities().toString().equals("[owner]"))
-            mav.setViewName("redirect:/dashboard");
-        else
-            mav.setViewName("redirect:/restaurant/1");
-        return mav;
+        return loginService.getMappingIndexPageDefaultSuccessLogin(authentication);
     }
 
     //Jika user mencoba mengakses ke page yang tidak seharusnya (role tidak sesuai) (misal cashier ingin mengakses dashboard)
     @GetMapping(value = "/privilege", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView privilegePage(Authentication authentication){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("privilege");
-        mav.addObject("privilege", "You don't have permission to access this page");
-        String username = authentication.getName();
-        mav.addObject("username", username);
-        String role = String.valueOf(authentication.getAuthorities());
-        mav.addObject("role", role);
-        return mav;
+        return loginService.getMappingAccessDeniedPage(authentication);
     }
 
 }
---
+++ src/main/java/project/blibli/mantapos/Service/LoginService.java
@@ -0,0 +1,45 @@
+package project.blibli.mantapos.Service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+@Service
+public class LoginService {
+
+    public ModelAndView getMappingLogin(String error,
+                                        String logout){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        //jika dalam URL itu ada parameter error (/login?error), maka ada error, maka tambahkan object error ke mav. Nantinya akan di-check di login.html
+        if(error!=null)
+            mav.addObject("error", "Invalid username and/or password");
+            //jika dalam URL itu ada parameter logout (/login?logout), maka user berhasil logout, maka tambahkan object logout ke mav. Nantinya akan di-check di login.html
+        else if(logout!=null)
+            mav.addObject("logout", "You've been logged out successfully!");
+        return mav;
+    }
+
+    public ModelAndView getMappingIndexPageDefaultSuccessLogin(Authentication authentication){
+        ModelAndView mav = new ModelAndView();
+        if(authentication.getAuthorities().toString().equals("[cashier]"))
+            mav.setViewName("redirect:/cashier");
+        else if(authentication.getAuthorities().toString().equals("[manager]") || authentication.getAuthorities().toString().equals("[owner]"))
+            mav.setViewName("redirect:/dashboard");
+        else
+            mav.setViewName("redirect:/restaurant/1");
+        return mav;
+    }
+
+    public ModelAndView getMappingAccessDeniedPage(Authentication authentication){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("privilege");
+        mav.addObject("privilege", "You don't have permission to access this page");
+        String username = authentication.getName();
+        mav.addObject("username", username);
+        String role = String.valueOf(authentication.getAuthorities());
+        mav.addObject("role", role);
+        return mav;
+    }
+
+}
